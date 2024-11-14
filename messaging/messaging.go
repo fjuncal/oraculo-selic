c7b79954047ab35f74ed21503a2e72cf20d123e5
Fellipe Juncal <fjuncal7@gmail.com>
2024-11-14 18:44:12 -0300

    Implementando metodo de reestabelecer conexao com a mensageria quando perde ou fica mto tempo no break point

--- messaging/messaging.go
+++ messaging/messaging.go
@@ -11,32 +11,57 @@ type Messaging interface {
 }
 
 type ActiveMQClient struct {
-	conn *stomp.Conn
+	conn     *stomp.Conn
+	queueURL string
 }
 
 func NewActiveMQClient(queueURL string) (*ActiveMQClient, error) {
-	conn, err := stomp.Dial("tcp", queueURL)
-	if err != nil {
+	client := &ActiveMQClient{queueURL: queueURL}
+	if err := client.connect(); err != nil {
 		return nil, err
 	}
-	return &ActiveMQClient{conn: conn}, nil
+	return client, nil
 }
 
+// SendMessage Função para enviar mensagem com reconexão automática
 func (c *ActiveMQClient) SendMessage(queueName, content string) error {
-	// Define o tipo de conteúdo como "text/plain"
-	err := c.conn.Send(
-		queueName,
-		"text/plain",
-		[]byte(content),
-	)
+	// Tenta enviar a mensagem
+	err := c.conn.Send(queueName, "text/plain", []byte(content))
 	if err != nil {
+		// Trata o erro de conexão fechada e tenta reconectar
 		log.Printf("Erro ao enviar mensagem para a fila %s: %v", queueName, err)
-		return err
+		log.Println("Tentando reconectar...")
+		if err := c.connect(); err != nil {
+			return err
+		}
+
+		// Após reconectar, tenta enviar a mensagem novamente
+		err = c.conn.Send(queueName, "text/plain", []byte(content))
+		if err != nil {
+			log.Printf("Erro ao enviar mensagem após reconexão para a fila %s: %v", queueName, err)
+			return err
+		}
 	}
 	log.Println("Mensagem enviada com sucesso para a fila:", queueName)
 	return nil
 }
 
+// Close Função para fechar a conexão
 func (c *ActiveMQClient) Close() error {
-	return c.conn.Disconnect()
+	if c.conn != nil {
+		return c.conn.Disconnect()
+	}
+	return nil
+}
+
+// connect Função para estabelecer ou restabelecer a conexão
+func (c *ActiveMQClient) connect() error {
+	conn, err := stomp.Dial("tcp", c.queueURL)
+	if err != nil {
+		log.Printf("Erro ao conectar ao ActiveMQ: %v", err)
+		return err
+	}
+	c.conn = conn
+	log.Println("Conexão estabelecida com sucesso com o ActiveMQ")
+	return nil
 }
