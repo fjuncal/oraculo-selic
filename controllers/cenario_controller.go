5a49d6fb950d88911ddbd1babba0850f69ae0f00
Fellipe Juncal <fjuncal7@gmail.com>
2024-11-29 01:39:40 -0300

    Implementando para carregar cenário por aba da planilha

--- controllers/cenario_controller.go
+++ controllers/cenario_controller.go
@@ -99,136 +99,150 @@ func (cc *CenarioController) UploadPlanilhaHandler(w http.ResponseWriter, r *htt
 	}
 	defer file.Close()
 
-	// Processar a planilha
-	cenario, passosTestes, err := cc.processarPlanilha(file)
+	// Processar a planilha e obter os cenários
+	cenarios, err := cc.processarPlanilha(file)
 	if err != nil {
 		log.Printf("Erro ao processar planilha: %v", err)
 		http.Error(w, "Erro ao processar planilha", http.StatusInternalServerError)
 		return
 	}
 
-	// Salvar o cenário
-	if err := cc.Repo.Save(&cenario); err != nil {
-		log.Printf("Erro ao salvar cenário: %v", err)
-		http.Error(w, "Erro ao salvar cenário", http.StatusInternalServerError)
-		return
-	}
-
-	// Salvar os passos testes e criar os relacionamentos
-	var relacionamentos []models.CenariosPassosTestes
-	for i, passo := range passosTestes {
-		// Salvar passo teste
-		if err := cc.Repo.PassoDB.SavePassoTeste(&passo); err != nil {
-			log.Printf("Erro ao salvar passo teste: %v", err)
-			http.Error(w, "Erro ao salvar passos testes", http.StatusInternalServerError)
+	// Iterar sobre os cenários e salvar no banco
+	for _, cenario := range cenarios {
+		// Salvar o cenário
+		if err := cc.Repo.Save(&cenario); err != nil {
+			log.Printf("Erro ao salvar cenário '%s': %v", cenario.Descricao, err)
+			http.Error(w, "Erro ao salvar cenários", http.StatusInternalServerError)
 			return
 		}
 
-		// Criar relacionamento
-		relacionamento := models.CenariosPassosTestes{
-			CenarioID:    cenario.ID,
-			PassoTesteID: passo.ID,
-			Ordenacao:    i + 1,
-		}
-		relacionamentos = append(relacionamentos, relacionamento)
-	}
+		// Salvar os passos testes e criar os relacionamentos
+		var relacionamentos []models.CenariosPassosTestes
+		for i, passo := range cenario.PassosTestes {
+			// Salvar passo teste
+			if err := cc.Repo.PassoDB.SavePassoTeste(&passo); err != nil {
+				log.Printf("Erro ao salvar passo teste '%s': %v", passo.Descricao, err)
+				http.Error(w, "Erro ao salvar passos testes", http.StatusInternalServerError)
+				return
+			}
 
-	// Salvar os relacionamentos
-	if err := cc.Repo.SaveOrUpdateRelacionamentos(relacionamentos); err != nil {
-		log.Printf("Erro ao salvar relacionamentos: %v", err)
-		http.Error(w, "Erro ao salvar relacionamentos", http.StatusInternalServerError)
-		return
+			// Criar relacionamento
+			relacionamento := models.CenariosPassosTestes{
+				CenarioID:    cenario.ID,
+				PassoTesteID: passo.ID,
+				Ordenacao:    i + 1,
+			}
+			relacionamentos = append(relacionamentos, relacionamento)
+		}
+
+		// Salvar os relacionamentos
+		if err := cc.Repo.SaveOrUpdateRelacionamentos(relacionamentos); err != nil {
+			log.Printf("Erro ao salvar relacionamentos para o cenário '%s': %v", cenario.Descricao, err)
+			http.Error(w, "Erro ao salvar relacionamentos", http.StatusInternalServerError)
+			return
+		}
 	}
 
 	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(cenario)
+	json.NewEncoder(w).Encode(cenarios)
 }
 
-func (cc *CenarioController) processarPlanilha(file multipart.File) (models.Cenario, []models.PassoTeste, error) {
+func (cc *CenarioController) processarPlanilha(file multipart.File) ([]models.Cenario, error) {
 	f, err := excelize.OpenReader(file)
 	if err != nil {
-		return models.Cenario{}, nil, fmt.Errorf("erro ao abrir o arquivo Excel: %v", err)
+		return nil, fmt.Errorf("erro ao abrir o arquivo Excel: %v", err)
 	}
 
 	// Obtém a lista de abas
 	sheets := f.GetSheetList()
 	if len(sheets) == 0 {
-		return models.Cenario{}, nil, fmt.Errorf("nenhuma aba encontrada na planilha")
+		return nil, fmt.Errorf("nenhuma aba encontrada na planilha")
 	}
 
 	log.Printf("Planilhas disponíveis: %v", sheets)
 
-	// Usar a primeira aba
-	rows, err := f.GetRows(sheets[0])
-	if err != nil {
-		return models.Cenario{}, nil, fmt.Errorf("erro ao ler linhas da planilha: %v", err)
+	var cenarios []models.Cenario
+
+	// Processa cada aba da planilha
+	for _, sheet := range sheets {
+		rows, err := f.GetRows(sheet)
+		if err != nil {
+			log.Printf("Erro ao ler linhas da aba '%s': %v", sheet, err)
+			continue
+		}
+
+		var passosTestes []models.PassoTeste
+		var headers map[string]int
+		var seqIndex int
+		var headersCenario map[string]int
+		var cenario models.Cenario
+
+		// Processar as linhas da aba
+		for i, row := range rows {
+			if len(row) == 0 {
+				// Ignorar linhas vazias
+				continue
+			}
+
+			if containsCabecalhoCenario(row) {
+				headersCenario = make(map[string]int)
+				for idx, col := range row {
+					headersCenario[col] = idx
+				}
+				log.Printf("Cabeçalhos de cenário identificados na aba '%s': %v", sheet, headersCenario)
+				continue
+			}
+
+			if containsLinhaDescricaoCenario(row) {
+				cenario = models.Cenario{
+					Descricao: getCellValue(row, headersCenario, "Descrição Cenário"),
+					Tipo:      getCellValue(row, headersCenario, "Tipo Cenário"),
+				}
+			}
+
+			if i == 0 || containsSeq(row) {
+				// Identifica a linha de cabeçalho
+				headers = make(map[string]int)
+				for idx, col := range row {
+					headers[col] = idx
+				}
+				log.Printf("Cabeçalhos identificados na aba '%s': %v", sheet, headers)
+				seqIndex = headers["Seq."]
+				continue
+			}
+
+			// Verifica se a linha é válida e contém informações suficientes
+			if len(row) < len(headers) || row[seqIndex] == "" {
+				log.Printf("Linha %d ignorada na aba '%s': %v", i, sheet, row)
+				continue
+			}
+
+			// Processa cada linha subsequente como um passo teste
+			passo := models.PassoTeste{
+				Descricao:        getCellValue(row, headers, "Descrição"),
+				TipoPassoTeste:   getCellValue(row, headers, "TipoPassoTeste"),
+				Canal:            getCellValue(row, headers, "Canal"),
+				CodigoMsg:        getCellValue(row, headers, "Operação"),
+				MsgDocXML:        getCellValue(row, headers, "MsgDocXML"),
+				Msg:              getCellValue(row, headers, "Msg"),
+				ContaCedente:     getCellValue(row, headers, "Conta Cedente"),
+				ContaCessionario: getCellValue(row, headers, "Conta Cessionária"),
+				NumeroOperacao:   getCellValue(row, headers, "Número Comando"),
+				Emissor:          getCellValue(row, headers, "Transmissor Debito"),
+				ValorFinanceiro:  parseFloat(getCellValue(row, headers, "Valor Financeiro")),
+				ValorPU:          parseFloat(getCellValue(row, headers, "PU")),
+			}
+			passosTestes = append(passosTestes, passo)
+		}
+
+		// Associa os passos testes ao cenário
+		if len(passosTestes) > 0 {
+			cenario.PassosTestes = passosTestes
+			cenarios = append(cenarios, cenario)
+		}
 	}
 
-	var passosTestes []models.PassoTeste
-	var headers map[string]int
-	var seqIndex int
-	var headersCenario map[string]int
-	var cenario models.Cenario
-
-	// Processar as linhas da planilha
-	for i, row := range rows {
-		if len(row) == 0 {
-			// Ignorar linhas vazias
-			continue
-		}
-
-		if containsCabecalhoCenario(row) {
-			headersCenario = make(map[string]int)
-			for idx, col := range row {
-				headersCenario[col] = idx
-			}
-			log.Printf("Cabeçalhos de cenário identificados: %v", headersCenario)
-			continue
-		}
-
-		if containsLinhaDescricaoCenario(row) {
-			cenario = models.Cenario{
-				Descricao: getCellValue(row, headersCenario, "Descrição Cenário"),
-				Tipo:      getCellValue(row, headersCenario, "Tipo Cenário"),
-			}
-		}
-
-		if i == 0 || containsSeq(row) {
-			// Identifica a linha de cabeçalho
-			headers = make(map[string]int)
-			for idx, col := range row {
-				headers[col] = idx
-			}
-			log.Printf("Cabeçalhos identificados: %v", headers)
-			seqIndex = headers["Seq."]
-			continue
-		}
-
-		// Verifica se a linha é válida e contém informações suficientes
-		if len(row) < len(headers) || row[seqIndex] == "" {
-			log.Printf("Linha %d ignorada: %v", i, row)
-			continue
-		}
-
-		// Processa cada linha subsequente como um passo teste
-		passo := models.PassoTeste{
-			Descricao:        getCellValue(row, headers, "Descrição"),
-			TipoPassoTeste:   getCellValue(row, headers, "TipoPassoTeste"),
-			Canal:            getCellValue(row, headers, "Canal"),
-			CodigoMsg:        getCellValue(row, headers, "Operação"),
-			MsgDocXML:        getCellValue(row, headers, "MsgDocXML"),
-			Msg:              getCellValue(row, headers, "Msg"),
-			ContaCedente:     getCellValue(row, headers, "Conta Cedente"),
-			ContaCessionario: getCellValue(row, headers, "Conta Cessionária"),
-			NumeroOperacao:   getCellValue(row, headers, "Número Comando"),
-			Emissor:          getCellValue(row, headers, "Transmissor Debito"),
-			ValorFinanceiro:  parseFloat(getCellValue(row, headers, "Valor Financeiro")),
-			ValorPU:          parseFloat(getCellValue(row, headers, "PU")),
-		}
-		passosTestes = append(passosTestes, passo)
-	}
-
-	return cenario, passosTestes, nil
+	return cenarios, nil
 }
 
 func containsLinhaDescricaoCenario(row []string) bool {
